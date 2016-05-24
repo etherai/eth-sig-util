75c7bda8618ae9fd8de85f6e98798ee0ada735aa
Mo Balaa <buddybalaa@gmail.com>
2016-05-24 18:29:20 +0000

    Fix typo

--- eth-sig-util.js
+++ eth-sig-util.js
@@ -1,5 +1,5 @@
 var verifyTx = require ('./lib/verifyTx.js');
-var verifyMsg = require ('./lib/verifyTx.js');
+var verifyMsg = require ('./lib/verifyMsg.js');
 
 module.exports = {   verifyTx : verifyTx,
                     verifyMsg : verifyMsg }
